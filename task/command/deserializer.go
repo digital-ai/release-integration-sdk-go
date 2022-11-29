2095b84b3183c2b61526416464d042c515f17803
gpugar <45165905+gpugar@users.noreply.github.com>
2022-11-29 12:02:53 +0100

    S-88849 Refactor command factory (#4)
    * Add command unmarshaller
    
    * S-88849 Refactor command creation to initialization
    
    * S-88849 Remove obsolete code
    
    Co-authored-by: Goran Pugar <goranpg@gmail.com>

--- task/command/deserializer.go
+++ task/command/deserializer.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/xebialabs/go-remote-runner-wrapper/task"
 	"k8s.io/klog"
-	"strconv"
 )
 
 type CommandType string
@@ -12,10 +11,6 @@ type CommandType string
 type CommandWrapper struct {
 	CommandType CommandType `json:"type"`
 	Properties  interface{}
-}
-
-type CommandFactory interface {
-	Create(commandType CommandType, properties []task.PropertyDefinition) (CommandExecutor, error)
 }
 
 func DeserializeCommand(factory CommandFactory, propertiesMap map[string]json.RawMessage) (CommandExecutor, error) {
@@ -36,90 +31,15 @@ func DeserializeCommand(factory CommandFactory, propertiesMap map[string]json.Ra
 		return nil, err
 	}
 
-	commandExec, err := factory.Create(wrapper.CommandType, properties)
+	spawnedCommand, err := factory.InitCommand(wrapper.CommandType)
+	if err != nil {
+		return nil, err
+	}
+
+	commandExec, err := PopulateCommand(spawnedCommand, properties)
 	if err != nil {
 		return nil, err
 	}
 
 	return commandExec, nil
-}
-
-func ExtractStringValue(property task.PropertyDefinition, field string) (*string, error) {
-	if property.Name == field && property.Value != nil {
-		unquoted, err := strconv.Unquote(string(property.Value))
-		if err != nil {
-			klog.Warningf("Error unmarshalling string value for %s with value [%s], skipping value.", field, property.Value)
-			return nil, nil
-		}
-		return &unquoted, nil
-	} else {
-		return nil, nil
-	}
-}
-
-func ExtractBoolValue(property task.PropertyDefinition, field string) (*bool, error) {
-	if property.Name == field && property.Value != nil {
-		boolVal, err := strconv.ParseBool(string(property.Value))
-		if err != nil {
-			klog.Warningf("Error unmarshalling bool value for %s with value [%s], skipping value.", field, property.Value)
-			return nil, nil
-		}
-		return &boolVal, nil
-	} else {
-		return nil, nil
-	}
-}
-
-func ExtractPropertiesStringValue(properties []task.PropertyDefinition, field string) (*string, error) {
-	for _, property := range properties {
-		if val, err := ExtractStringValue(property, field); val != nil {
-			return val, nil
-		} else if err != nil {
-			return nil, err
-		}
-	}
-	return nil, nil
-}
-
-func ExtractPropertiesStringArray(properties []task.PropertyDefinition, field string) (*[]string, error) {
-	for _, property := range properties {
-		if property.Name != field {
-			continue
-		}
-		var array []string
-		if err := json.Unmarshal(property.Value, &array); err != nil {
-			klog.Warningf("Error unmarshalling string array value for %s with value [%s], skipping value.", field, property.Value)
-			return nil, err
-		}
-		return &array, nil
-	}
-	return nil, nil
-}
-
-func ExtractPropertiesMapStringString(properties []task.PropertyDefinition, field string) (*map[string]string, error) {
-	for _, property := range properties {
-		if property.Name != field {
-			continue
-		}
-		var keyValues map[string]string
-		if err := json.Unmarshal(property.Value, &keyValues); err != nil {
-			klog.Warningf("Error unmarshalling map[string]string value for %s with value [%s], skipping value.", field, property.Value)
-			return nil, err
-		}
-		return &keyValues, nil
-	}
-	return nil, nil
-}
-
-func ExtractIntValue(property task.PropertyDefinition, field string) (*int64, error) {
-	if property.Name == field && property.Value != nil {
-		intVal, err := strconv.ParseInt(string(property.Value), 10, 0)
-		if err != nil {
-			klog.Warningf("Error unmarshalling int value for %s with value [%s], skipping value.", field, property.Value)
-			return nil, nil
-		}
-		return &intVal, nil
-	} else {
-		return nil, nil
-	}
 }
