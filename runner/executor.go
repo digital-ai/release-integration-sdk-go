5417a86de7263b697b4b2919a696ac3c0c7dc3ca
Goran Pugar <goranpg@gmail.com>
2022-12-22 09:52:25 +0100

    Refactor runners

--- runner/executor.go
+++ runner/executor.go
@@ -3,18 +3,80 @@ package runner
 import (
 	"fmt"
 	"github.com/xebialabs/go-remote-runner-wrapper/task"
+	"github.com/xebialabs/go-remote-runner-wrapper/task/command"
 	"k8s.io/klog"
 	"os"
+	"strconv"
 )
 
-type runFn func(task.InputContext) (map[string]interface{}, error)
+type RunFunction func(task.InputContext) (map[string]interface{}, error)
+type FactoryBuilder func(task task.TaskContext) (command.CommandFactory, error)
 
-var InputLocation = os.Getenv("INPUT_LOCATION")
-var OutputLocation = os.Getenv("OUTPUT_LOCATION")
+type Runner interface {
+	Run(task.InputContext) (map[string]interface{}, error)
+}
 
-func Execute(pluginVersion string, buildDate string, run runFn) {
+type SimpleRunner struct {
+	run RunFunction
+}
+
+type CommandRunner struct {
+	factoryBuilder    FactoryBuilder
+	ResultField       string
+	SecureResultField string
+}
+
+func NewSimpleRunner(run RunFunction) *SimpleRunner {
+	var runner SimpleRunner
+	runner.run = run
+	return &runner
+}
+
+func (runner SimpleRunner) Run(ctx task.InputContext) (map[string]interface{}, error) {
+	return runner.run(ctx)
+}
+
+func NewCommandRunner(factoryBuilder FactoryBuilder, resultField string, securedResultField string) *CommandRunner {
+	var runner CommandRunner
+	runner.factoryBuilder = factoryBuilder
+	runner.ResultField = resultField
+	runner.SecureResultField = securedResultField
+	return &runner
+}
+
+func (runner CommandRunner) Run(ctx task.InputContext) (map[string]interface{}, error) {
+	factory, err := runner.factoryBuilder(ctx.Task)
+	if err != nil {
+		return nil, err
+	}
+	exec, err := command.DeserializeCommand(factory, ctx.Task)
+	if err != nil {
+		return nil, err
+	}
+
+	result, masked, err := exec.FetchResult()
+	if err != nil {
+		klog.Infof("Finished executing command with error %v", err)
+		return command.MakeFailCommandExecutionResultMap(err), nil
+	}
+	klog.Infoln("Finished executing command")
+	if masked {
+		secureResponse, convErr := strconv.Unquote(string(result))
+		if convErr != nil {
+			klog.Infof("Failed to mask secure command with error %v", convErr)
+			return command.MakeFailCommandExecutionResultMap(convErr), nil
+		}
+		return command.MakeSuccessSecureCommandExecutionResultMap(runner.SecureResultField, secureResponse), nil
+	}
+	return command.MakeSuccessCommandExecutionResultMap(runner.ResultField, result), nil
+}
+
+func Execute(pluginVersion string, buildDate string, runner Runner) {
 	klog.Infof("PluginVersion:\t%s", pluginVersion)
 	klog.Infof("BuildDate:\t%s", buildDate)
+
+	var InputLocation = os.Getenv("INPUT_LOCATION")
+	var OutputLocation = os.Getenv("OUTPUT_LOCATION")
 
 	var taskContext task.InputContext
 	if err := task.Deserialize(InputLocation, &taskContext); err != nil {
@@ -23,7 +85,7 @@ func Execute(pluginVersion string, buildDate string, run runFn) {
 		return
 	}
 
-	executionResult, err := run(taskContext)
+	executionResult, err := runner.Run(taskContext)
 	if err != nil {
 		klog.Errorf("Failed executing runner function %v", err)
 		task.SerializeError(OutputLocation, fmt.Errorf("failed to execute run function: %w", err))
