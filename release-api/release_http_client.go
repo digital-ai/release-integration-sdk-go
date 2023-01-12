a3d88ad3c49ca9b08add04cc02c4466446f78f91
ivona13 <ivona.raguz13@gmail.com>
2023-01-12 15:23:45 +0100

    S-89601 Add initial implementation of release api

--- release-api/release_http_client.go
+++ release-api/release_http_client.go
@@ -3,14 +3,13 @@ package release_api
 import (
 	"github.com/xebialabs/go-remote-runner-wrapper/http"
 	"github.com/xebialabs/go-remote-runner-wrapper/task"
-	"path"
 )
 
 func GetReleaseServerHttpClient(ctx task.ReleaseContext) (*http.HttpClient, error) {
 	auth := ctx.AutomatedTaskAsUser
 
 	httpClientConfig := &http.HttpClientConfig{
-		Host: path.Join(ctx.Url, "api/v1"),
+		Host: ctx.Url,
 		BasicAuthentication: &http.BasicAuthentication{
 			Username: auth.Username,
 			Password: auth.Password,
