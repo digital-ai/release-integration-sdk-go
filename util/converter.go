71e5a2da221883499dc2868d0bc0146c3d5f2958
Marin Milina <marin.milina@ag04.com>
2023-02-20 15:40:47 +0100

    S-90023 Add method for converting yaml to json

---
+++ util/converter.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"encoding/json"
+	"gopkg.in/yaml.v3"
+)
+
+func YamlToJson(yamlData []byte) ([]byte, error) {
+	var data interface{}
+	err := yaml.Unmarshal(yamlData, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
