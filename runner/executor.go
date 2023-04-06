48117c24c4358757ad32c14709cd295050235360
gpugar <45165905+gpugar@users.noreply.github.com>
2023-04-06 14:34:02 +0200

    S-91319 Remove Comment and Status entries from SDK (#30)
    Co-authored-by: Goran Pugar <goranpg@gmail.com>

--- runner/executor.go
+++ runner/executor.go
@@ -49,12 +49,7 @@ func (runner CommandRunner) Run(ctx task.InputContext) *task.Result {
 	if err != nil {
 		return returnResult.Error(fmt.Errorf("cannot deserialize input: %v", err))
 	}
-
-	task.Comment("Starting execution")
-	task.Status("Starting execution")
 	result, err := exec.FetchResult()
-	task.Comment("Ending execution")
-	task.Status("Ending execution")
 	if err != nil {
 		klog.Infof("Finished executing command with error %v", err)
 		if result != nil {
@@ -69,9 +64,6 @@ func (runner CommandRunner) Run(ctx task.InputContext) *task.Result {
 func Execute(pluginVersion string, buildDate string, runner Runner) {
 	klog.Infof("PluginVersion:\t%s", pluginVersion)
 	klog.Infof("BuildDate:\t%s", buildDate)
-	task.Comment("Preparation phase")
-	task.Status("Preparation phase")
-
 	var taskContext task.InputContext
 	if err := task.Deserialize(&taskContext); err != nil {
 		klog.Errorf("Failed to deserialize input %v", err)
@@ -80,9 +72,6 @@ func Execute(pluginVersion string, buildDate string, runner Runner) {
 	}
 
 	logger.AddSecrets(taskContext)
-
-	task.Comment("Output phase")
-	task.Status("Output phase")
 	executionResult := runner.Run(taskContext)
 
 	resultMap, err := executionResult.Get()
