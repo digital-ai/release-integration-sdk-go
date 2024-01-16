a7e198c46e7d6d32cc3cc476e377bb38c507fbde
mkeretic1 <keretic.mateo@gmail.com>
2024-01-16 13:52:28 +0100

    S-104721 Add support for handling large results on remote runner (#52)
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 development commit
    
    * S-104721 Make push retry infinite
    
    * S-104721 Make push retry infinite
    
    * S-104721 Always retry callback request when result is bigger than 1Mb
    
    * S-104721 Improve callback retry
    
    * S-104721 Remove TODO comment
    
    * S-104721 make retryDelay calculation one-liner

--- task/result-handler.go
+++ task/result-handler.go
@@ -10,10 +10,14 @@ import (
 	"github.com/digital-ai/release-integration-sdk-go/k8s"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog/v2"
+	"math"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
+
+const SizeOf1Mb = 1024 * 1024
 
 // callbackUrl is the environment variable for the callback URL to push results.
 var callbackUrl = os.Getenv(CallbackURL)
@@ -69,8 +73,9 @@ func handleResult(outputContext TaskOutputContext) {
 
 	done := make(chan string, 3)
 	success := make(chan bool)
+
 	go func() {
-		err := pushResult(encryptedData)
+		err := pushResult(encryptedData, shouldRetryCallbackRequest(encryptedData))
 		handleResultHandlerError("HTTP Push", done, success, err)
 	}()
 	go func() {
@@ -116,6 +121,9 @@ func splitSecretResourceData(secretEntry string) (string, string, string, error)
 // writeToSecret writes the encrypted data to the result secret if the resultSecretKey is set.
 func writeToSecret(encryptedData []byte) error {
 	if len(resultSecretKey) > 0 {
+		if len(encryptedData) >= SizeOf1Mb {
+			return errors.New("result size exceeds 1Mb and is too big to store in secret")
+		}
 		namespace, name, key, err := splitSecretResourceData(resultSecretKey)
 		if err != nil {
 			klog.Warningf("Cannot resolve value of Result Secret Name and Key %s, skipping - output written to output file", err)
@@ -148,7 +156,7 @@ func writeToSecret(encryptedData []byte) error {
 }
 
 // pushResult pushes the encrypted data to the callback URL if the callbackUrl is set.
-func pushResult(encryptedData []byte) error {
+func pushResult(encryptedData []byte, retryOnFailure bool) error {
 	if len(callbackUrl) > 0 {
 		callBackUrl, err := base64.StdEncoding.DecodeString(callbackUrl)
 		if err != nil {
@@ -156,11 +164,16 @@ func pushResult(encryptedData []byte) error {
 			return err
 		}
 		url := string(callBackUrl)
-		// TODO retry schema maybe?
 		response, httpError := http.Post(url, "application/json", bytes.NewReader(encryptedData))
 		if httpError != nil {
-			klog.Warningf("Cannot finish Callback request: %s, skipping", httpError)
-			return httpError
+			klog.Warningf("Cannot finish Callback request: %s", httpError)
+			if retryOnFailure {
+				klog.Infof("Retry flag was set on Callback request, retrying until successful...")
+				err = retryPushResultInfinitely(encryptedData)
+				return err
+			} else {
+				return httpError
+			}
 		}
 		if response.StatusCode != 204 {
 			klog.Warningf("Got NOK HTTP Status Code %s, skipping", response.Status)
@@ -169,6 +182,44 @@ func pushResult(encryptedData []byte) error {
 		return nil
 	} else {
 		return new(SkipResultHandler)
+	}
+}
+
+// retryPushResultInfinitely keeps retrying to push encrypted data to the callback URL.
+func retryPushResultInfinitely(encryptedData []byte) error {
+	retryDelay := 1 * time.Second
+	maxBackoff := 3 * time.Minute
+	backoffFactor := 2.0
+
+	for {
+		clientset, err := k8s.GetClientset()
+		if err != nil {
+			klog.Warningf("Cannot create clientset for handling Result Secret: %s", err)
+			return err
+		}
+		// we need to re-fetch callback URL since it will change when remote-runner restarts
+		secret, err := clientset.CoreV1().Secrets(os.Getenv(RunnerNamespace)).Get(context.Background(), os.Getenv(InputContextSecret), v1.GetOptions{})
+		if err != nil {
+			klog.Warningf("Cannot fetch Result Secret: %s", err)
+			return err
+		}
+		callbackUrl = string(secret.Data[InputContextSecretDataUrlKey])
+		decodedCallbackUrl, err := base64.StdEncoding.DecodeString(callbackUrl)
+		if err != nil {
+			klog.Warningf("Cannot decode Callback URL %s", err)
+			return err
+		}
+		url := string(decodedCallbackUrl)
+
+		_, httpError := http.Post(url, "application/json", bytes.NewReader(encryptedData))
+		if httpError == nil {
+			return nil
+		}
+
+		klog.Warningf("Cannot finish retried Callback request: %s. Retrying in %v...", httpError, retryDelay)
+		time.Sleep(retryDelay)
+
+		retryDelay = time.Duration(math.Min(float64(retryDelay)*backoffFactor, float64(maxBackoff)))
 	}
 }
 
@@ -185,4 +236,10 @@ func writeOutput(encryptedData []byte) error {
 	} else {
 		return new(SkipResultHandler)
 	}
+}
+
+// shouldRetryCallbackRequest checks if callback request should be retried on failure.
+// it should be retried when result is too big for Secret and Output File handler is not used
+func shouldRetryCallbackRequest(encryptedData []byte) bool {
+	return len(encryptedData) >= SizeOf1Mb && len(os.Getenv(OutputLocation)) == 0
 }
