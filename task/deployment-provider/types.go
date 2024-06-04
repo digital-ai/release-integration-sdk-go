e875a45c782d33960eae5be0b6fa3cfafcadc0de
mkeretic1 <keretic.mateo@gmail.com>
2024-06-04 14:10:59 +0200

    S-109550 Implement config filtering for Deploy status scripts (#64)
    * S-107984 Add support for CI serialization
    
    * S-107984 Add nil for JSON CI.id serialiation
    
    * S-108775 Add configId to DeploymentProviderEvent (#57)
    
    * Add documentation comments
    
    ---------
    
    Co-authored-by: Goran Pugar <goranpg@gmail.com>
    Co-authored-by: gpugar <45165905+gpugar@users.noreply.github.com>

---
+++ task/deployment-provider/types.go
@@ -0,0 +1,74 @@
+package deployment_provider
+
+import (
+	"github.com/digital-ai/release-integration-sdk-go/task/ci"
+	"reflect"
+	"time"
+)
+
+func init() {
+	ci.RegisterTypeMappings(ci.TypeMap{
+		reflect.TypeOf(Environment{}):             "xlrelease.Environment",
+		reflect.TypeOf(Application{}):             "xlrelease.Application",
+		reflect.TypeOf(DeploymentState{}):         "xlrelease.DeploymentState",
+		reflect.TypeOf(DeploymentProviderEvent{}): "events.DeploymentProviderEvent",
+	})
+}
+
+type DeploymentProviderEvent struct {
+	Operation       string          `synthetic:"operation"`
+	Environment     Environment     `synthetic:"environment"`
+	Application     Application     `synthetic:"application"`
+	DeploymentState DeploymentState `synthetic:"deploymentState"`
+	ConfigId        string          `synthetic:"configId"`
+}
+
+type Environment struct {
+	Title            string           `synthetic:"title"`
+	CorrelationUid   string           `synthetic:"correlationUid"`
+	DeploymentTarget DeploymentTarget `synthetic:"deploymentTarget"`
+}
+
+type Application struct {
+	Title             string            `synthetic:"title"`
+	CorrelationUid    string            `synthetic:"correlationUid"`
+	ApplicationSource ApplicationSource `synthetic:"applicationSource"`
+}
+
+type DeploymentState struct {
+	StatusGroup    string    `synthetic:"statusGroup"`
+	Status         string    `synthetic:"status"`
+	VersionTag     string    `synthetic:"versionTag"`
+	VersionState   string    `synthetic:"versionState"`
+	DeploymentType string    `synthetic:"deploymentType"`
+	User           string    `synthetic:"user"`
+	LastChangeTime time.Time `synthetic:"lastChangeTime"`
+}
+
+type ApplicationSourceType struct {
+	ServerUrl string `synthetic:"serverUrl"`
+}
+
+type DeploymentTargetType struct {
+	TargetUrl string `synthetic:"targetUrl"`
+}
+
+type ComposedType interface {
+	Type() reflect.Type
+}
+
+type ApplicationSource interface {
+	ComposedType
+}
+
+type DeploymentTarget interface {
+	ComposedType
+}
+
+func RegisterApplicationSourceType(typeName string) {
+	ci.RegisterTypeMapping(reflect.TypeOf(ApplicationSourceType{}), typeName)
+}
+
+func RegisterDeploymentTargetType(typeName string) {
+	ci.RegisterTypeMapping(reflect.TypeOf(DeploymentTargetType{}), typeName)
+}
